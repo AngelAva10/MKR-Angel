419a9f3ac497d81d61a3be3c2daee4e425d2378d
Angel <angelviveros92@gmail.com>
2023-02-13 22:30:21 -0300

    deleted

--- workshop-mkr/src/components/utils/users.js
+++
@@ -1,4 +0,0 @@
-export const user = {
-  email: 'test@email',
-  password: '1234'
-};
--- workshop-mkr/src/pages/Tasks.jsx
+++
@@ -1,6 +0,0 @@
-import { TasksList } from '../components/Tasks/TasksList';
-
-export const Tasks = () => {
-  return <TasksList />;
-  // return isLoggedIn && <TasksList />;
-};
